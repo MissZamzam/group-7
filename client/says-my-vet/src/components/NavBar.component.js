ca9da3cb45c3631030818bb925d2f120fdf76ece
leroy-netizen <leroythomas051@gmail.com>
2022-09-02 10:30:59 +0300

    add navBar

---
+++ client/says-my-vet/src/components/NavBar.component.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+      <div>
+          <div className="logo">
+              <h1>Says_my_vet</h1>
+          </div>
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Register_doctpr</li>
+              <li>view-doctors</li>
+              <li>Blog/resources</li>
+          </ul>
+    </div>
+  )
+}
+
+export default NavBar
