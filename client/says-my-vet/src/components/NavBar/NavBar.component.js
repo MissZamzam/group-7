1ec936d3016d9ef45287cfd6e89ac66b553865e2
Tracie35 <kariukitracie@gmail.com>
2022-09-02 17:10:19 +0300

    stylling Navbar

--- client/says-my-vet/src/components/NavBar/NavBar.component.js
+++ client/says-my-vet/src/components/NavBar/NavBar.component.js
@@ -3,18 +3,22 @@ import './NavBar.styles.css'
 
 const NavBar = () => {
   return (
-      <div>
+      <div className="nav_bar">
           <div className="logo">
-            <h1>Pets first</h1>  
+            <h1>Pets <em>First</em></h1>  
           </div>
-          <ul>
+          <div className='routes'> <ul>
               <li>Home</li>
               <li>About</li>
               <li>Register_doctor</li>
               <li>View_doctors</li>
               <li>Pets data</li>
           </ul>
-    </div>
+
+          </div>
+         
+          </div>
+    
   )
 }
 
