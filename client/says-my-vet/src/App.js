ca5fcba21013b597fc97ce44d8eb09e337cc7a89
leroy-netizen <leroythomas051@gmail.com>
2022-09-02 12:26:55 +0300

    add routing

--- client/says-my-vet/src/App.js
+++ client/says-my-vet/src/App.js
@@ -1,13 +1,33 @@
+
 import NavBar from './components/NavBar/NavBar.component.js'
 import Footer from './components/Footer/Footer.component.js'
+// import pages 
+import Home from './pages/Home/Home.page.js';
+import About from './pages/About/About.page.js';
+import Blog from './pages/Blog/Blog.page.js';
+import RegisterDoc from './pages/RegisterDoc/RegDoc.page.js';
+import ViewDoc from './pages/ViewDoc/ViewDoc.page.js';
+// absolute imports
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>SAys my vet</h1>
+    <Router>
       <NavBar />
+       <div className="App">
+      <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path='/about' element={<About/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            <Route path='/registerdoc' element={<RegisterDoc/>}/>
+            <Route path='/viewdoc' element={<ViewDoc/>}/>
+      {/* <h1>Says my vet</h1> */}
+      </Routes>
       <Footer/>
     </div>
+      </Router>
+   
   );
 }
 
