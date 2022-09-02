98a78359b2baff480bcd3a37148fd404d23df5b5
leroy-netizen <leroythomas051@gmail.com>
2022-09-02 10:58:29 +0300

    refactor

--- client/says-my-vet/src/App.js
+++ client/says-my-vet/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import NavBar from './components/NavBar/NavBar.component.js'
+import Footer from './components/Footer/Footer.component.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>SAys my vet</h1>
-      <NavBar/>
+      <NavBar />
+      <Footer/>
     </div>
   );
 }
