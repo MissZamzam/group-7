78bc7353eac5679e041ca86419069243f9b16e13
Zamzam <Zamzamhassan44@gmail.com>
2022-09-03 21:10:10 +0300

    server flder updated

--- client/says-my-vet/src/pages/Blog/Blog.page.js
+++ client/says-my-vet/src/pages/Blog/Blog.page.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className='resource'>
+    <h2 className='heading'>BlogPage/Resources</h2>
+
+    <div className='dog_disease'>
+      <h2>Diseases</h2>
+      <a href="https://www.avma.org/resources-tools/pet-owners/petcare/disease-risks-dogs-social-settings">Disease risks for dogs in social settings</a>
+    </div>
+
+  </div>
   )
 }
 
