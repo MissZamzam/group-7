2e23e098ecb14114fae26aa946d97c91ecad9632
Zamzam <Zamzamhassan44@gmail.com>
2022-09-04 12:43:16 +0300

    added four resources to the dog food category

--- client/says-my-vet/src/pages/Blog/Blog.page.js
+++ client/says-my-vet/src/pages/Blog/Blog.page.js
@@ -20,7 +20,12 @@ const BlogPage = () => {
       <h2>Dog Food</h2>
       <a href="https://www.akc.org/expert-advice/nutrition/how-many-times-a-day-should-a-dog-eat/#:~:text=In%20general%2C%20dogs%20should%20be,kibble%20or%20wet%20canned%20food.">How Often Should Dogs Eat?</a>
       <br></br>
-      
+      <a href="https://www.thesprucepets.com/dog-food-basics-1118454">Dog Food Basics</a>
+      <br></br>
+      <a href="https://vcahospitals.com/know-your-pet/nutrition-general-feeding-guidelines-for-dogs">Nutrition - General Feeding Guidelines for Dogs</a>
+      <br></br>
+      <a href="https://healthyhomemadedogtreats.com/top-42-dog-food-facts-every-caring-dog-owner-must-know/">42 Dog Food Facts Every Caring Dog Owner Must Know</a>
+
     </div>
 
   </div>
