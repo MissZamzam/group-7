9a996ed387b5f0ecb9707663005a9eeec69f5276
Zamzam <Zamzamhassan44@gmail.com>
2022-09-04 20:03:35 +0300

    added select and button input to the registration form

--- client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
+++ client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
@@ -23,7 +23,37 @@ const RegDoc = () => {
                         <option value="4">Kisumu</option>
                     </select>              
                   </div>
-              
+                  <div class="input-box">
+                    <span class="details">Specialization</span>
+                    <select name="" id="" >
+                        <option value="1">Dentistry</option>
+                        <option value="2">Nutrition</option>
+                        <option value="3">Theriogenology</option>
+                        <option value="4">Ophtalmology</option>
+                    </select>
+                </div>
+                <div class="input-box">
+                    <span class="details">Phone</span>
+                    <input type="text" placeholder="Enter your number" required />
+                </div>
+                <div class="input-box">
+                    <span class="details">Picture Link</span>
+                    <input type="text" placeholder="link to face image" required />
+                </div>
+                <div class="input-box">
+                    <span class="details">Years of Experience</span>
+                    <select name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6 and more</option>
+                    </select>
+                    </div>
+                    <div class="button" id="btn">
+                        <input type="submit" value="Register" />
+                    </div>
         </div>
         <form action="#"></form>
     </div>
