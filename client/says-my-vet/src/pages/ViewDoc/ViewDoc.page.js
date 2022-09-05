4b4381fb40232f6d536ffe810c633545c9049144
leroy-netizen <leroythomas051@gmail.com>
2022-09-05 07:42:36 +0300

    view doctors

--- client/says-my-vet/src/pages/ViewDoc/ViewDoc.page.js
+++ client/says-my-vet/src/pages/ViewDoc/ViewDoc.page.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import './ViewDoc.styles.css';
-import image from '../../assets/img/dog.jpg'
+// import image from '../../assets/img/dog.jpg'
 import {useState, useEffect} from 'react'
-
 const ViewDoctors = () => {   
-    const url = 'http://localhost:9292/doctors'
-
+    
     const [doctors, setDoctors] = useState([]);
+    
+    const handleLocationChange = (e) => {
+        console.log(e.target.value)
+        
+    }
+    const url = 'http://localhost:9292/doctors'
+    
+  
 
     const getDocttorsData = async () => {
         const response = await fetch(url)
@@ -27,41 +33,39 @@ const ViewDoctors = () => {
 
                     {/* filter by location */}
               <label htmlFor="location">Location:</label>
-              <select name="location" id="location">
-                  <option value="location-1">Location-1</option>
-                  <option value="location-2">Location-2</option>
-                  <option value="location-3">Location-3</option>
-                  <option value="location-4">Location-4</option>
-                  <option value="location-5">Location-5</option>
-                </select>
+
+                        <select name="location" id="location">
+                            {doctors.map(doctor => (
+                                <option value="location-2">{ <p>{doctor.location}</p> }</option>
+                           ))}
+                         </select>
+            
                     </div>
                     <div className='select-container'>
                         
                     {/* filter by years of experience */}
               <label htmlFor="experience">Experience:</label>
-              <select name="experience" id="experience">
-                  <option value="experience-1">experience-1</option>
-                  <option value="experience-2">experience-2</option>
-                  <option value="experience-3">experience-3</option>
-                  <option value="experience-4">experience-4</option>
-                  <option value="experience-5">experience-5</option>
+                        <select name="experience" id="experience">
+                             {doctors.map(doctor => (
+                                <option value="experience">{ <p>{doctor.years_of_experience}</p> }</option>
+                           ))}
+       
                 </select>
                     </div>
                     <div className='select-container'>
 
                     {/* filter by specialization */}
               <label htmlFor="specialization">Specialization:</label>
-              <select name="location" id="location">
-                  <option value="specialization-1">specialization-1</option>
-                  <option value="specialization-2">specialization-2</option>
-                  <option value="specialization-3">specialization-3</option>
-                  <option value="specialization-4">specialization-4</option>
-                  <option value="specialization-5">specialization-5</option>
+                        <select name="location" id="location" onChange={handleLocationChange}>
+                              {doctors.map(doctor => (
+                                <option value="experience">{ <p>{doctor.specialization}</p> }</option>
+                           ))}
+               
                 </select>
                     </div>
           </div>      
                 <div className="main-cards">
-
+ 
                     {
                         doctors && doctors.map((doctor) => {
                             return (
@@ -69,7 +73,7 @@ const ViewDoctors = () => {
                         <div className="location">
                                         <p>location: { doctor.location}</p>
                         </div>
-                        <div className="card-img"><img src={ doctor.picture_link} alt="dog-image" /></div>
+                        <div className="card-img"><img src={ doctor.picture_link} alt={doctor.name} /></div>
                         <hr />
                         <div className="card-text">
                                         <p>Name: { doctor.name}</p>
@@ -80,39 +84,7 @@ const ViewDoctors = () => {
                         })
                     }
                     
-                    {/* <div className="card-container">
-                        <div className="location">
-                            <p>location</p>
-                        </div>
-                        <div className="card-img"><img src={ image} alt="dog image" /></div>
-                        <hr />
-                        <div className="card-text">
-                            <p>Name:</p>
-                            <p>Phone_no:</p>
-                        </div>
-                </div> */}
-                    {/* <div className="card-container">
-                        <div className="location">
-                            <p>location</p>
-                        </div>
-                        <div className="card-img"><img src={ image} alt="dog image" /></div>
-                        <hr />
-                        <div className="card-text">
-                            <p>Name:</p>
-                            <p>Phone_no:</p>
-                        </div>
-                </div> */}
-                    {/* <div className="card-container">
-                        <div className="location">
-                            <p>location</p>
-                        </div>
-                        <div className="card-img"><img src={ image} alt="dog image" /></div>
-                        <hr />
-                        <div className="card-text">
-                            <p>Name:</p>
-                            <p>Phone_no:</p>
-                        </div>
-                </div> */}
+                   
                 </div>      
       </div>
       </>
