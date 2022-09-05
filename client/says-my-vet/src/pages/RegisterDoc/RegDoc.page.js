ec80aeb73c3a675eee591a18b1b0357eebf0cc78
leroy-netizen <leroythomas051@gmail.com>
2022-09-05 16:18:26 +0300

    get input values by introducing controlled components

--- client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
+++ client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./RegDoc.styles.css"
 
 const RegDoc = () => {
+  // state for controlled input fields
+  const [name, setName] = useState('')
+  const [licenceNo, setLicenceNo] = useState('')
+  const [location, setLocation] = useState('Nairobi')
+  const [specialization, setSpecialization] = useState('Dentistry')
+  const [phone, setPhone] = useState('')
+  const [picLink, setPicLink] = useState('')
+  const [experience, setExperience]= useState('')
   return (
 <div className='registerDoctors'>
   <form className="myForm" > 
@@ -9,15 +17,15 @@ const RegDoc = () => {
     <div className="register_doctors">
       <div className='register_doctorsA'>
         <label>Name:
-          <input type="text" className="field" placeholder="name" name="name" />
+          <input type="text" className="field" placeholder="name" value={name} onChange={(e)=> setName(e.target.value) } />
         </label>
         <label>License_number:
-          <input type="text"  className="field" placeholder="license_number" name="age" />
+              <input type="text" className="field" placeholder="license_number" value={licenceNo} onChange={ (e)=> setLicenceNo(e.target.value)} />
         </label>
       </div>
     <div className='register_doctorsA'>
         <label id='location'>Location: 
-          <select  name="location">
+          <select  value={location} onChange={(e)=> setLocation(e.target.value)} >
             <option value="Nairobi">Nairobi</option>
             <option value="Mombasa">Mombasa</option>
             <option value="Nakuru">Nakuru</option>
@@ -25,7 +33,7 @@ const RegDoc = () => {
           </select>
         </label>
         <label id='specialization'>Specialization:
-          <select name="service_specialized" id="specialize_id" >
+              <select value={specialization} onChange={ (e)=>setSpecialization(e.target.value)} id="specialize_id" >
             <option value="Dentistry">Dentistry</option>
             <option value="Dermatology">Dermatology</option>
             <option value="Nutrition">Nutrition</option>
@@ -36,13 +44,15 @@ const RegDoc = () => {
     </div>
     <div className='register_doctorsA'>
     <label>Phone Number:
-    <input type="text" name="phone_number"  className="field" placeholder="phone_number" /> </label>
+              <input type="text" value={phone} className="field" placeholder="phone_number" onChange={(e) => setPhone(e.target.value)} />
+            </label>
     <label>Picture Link:
-    <input type="text" name="pictureLink"  className="field" placeholder="link to face-image"/></label>
+              <input type="text" value={picLink} className="field" placeholder="link to face-image" onChange={(e) => setPicLink(e.target.value)} />
+    </label>
     </div>
     <div className='register_doctorsA'>
     <label>Years of Experience
-    <select name="years_of_experience">
+    <select value={experience} onChange={(e)=> {setExperience(e.target.value)}}>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -53,10 +63,18 @@ const RegDoc = () => {
     </label>
     </div>
     <div className='submitBtn'>
-    <input type="submit" className="registerbtn"/>    
+    <button type="submit" className="registerbtn"> Submit</button>    
     </div>                  
       </div>
-  </form>        
+      </form> 
+      <p>{name}</p>
+      <p>{licenceNo}</p>
+      <p>{picLink}</p>
+      <p>{phone}</p>
+      <p>{specialization}</p>
+      <p>{location}</p>
+      <p>{experience}</p>
+      
 </div>
   )
 }
