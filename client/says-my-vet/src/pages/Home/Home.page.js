2ddb3859a7474069432d6661ea7e5a12e3e1451d
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-05 20:37:12 +0300

    add functionality to learn more button

--- client/says-my-vet/src/pages/Home/Home.page.js
+++ client/says-my-vet/src/pages/Home/Home.page.js
@@ -1,13 +1,24 @@
-import React from 'react'
+// import React, { useState } from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 import '../Home/Home.style.css'
 import { FaCat, FaChevronCircleRight, FaCircle, FaDog, FaStethoscope } from "react-icons/fa";
 
-const Home = () => {
+const Home = () =>
+{
+  // const [ submit, onSubmit ] = useState(e.target.value )
+  
+  // function onHandleSubmit (e)
+  // {
+  //   onSubmit((submit)=> submit)
+  // }
   return (
     <div className="home-page">
       <div className="home-sec1">
         <h3 id="descr">High Quality Care For Pets You'll Love</h3>
-        <button className="info-btn">LEARN MORE</button>
+        <Link to = '/about'>
+          <button className="info-btn">LEARN MORE</button>
+        </Link>
       </div>
       <div className="home-sec2">
         <div className="card1">
@@ -18,9 +29,9 @@ const Home = () => {
 
           <h4>Pet Walking</h4>
           <p>
-            Welcome to Pets First web page. We are committed
-            to enhance your health by ensuring that you are always safe and have
-            a happy companion (Pet).
+            Welcome to Pets First web page. We are committed to enhance your
+            health by ensuring that you are always safe and have a happy
+            companion (Pet).
           </p>
           <div>
             <FaChevronCircleRight className="icon-4" />
@@ -33,9 +44,9 @@ const Home = () => {
 
           <h4>Pet Daycare</h4>
           <p>
-            Welcome to Pets First web page. We are committed
-            to enhance your health by ensuring that you are always safe and have
-            a happy companion (Pet).
+            Welcome to Pets First web page. We are committed to enhance your
+            health by ensuring that you are always safe and have a happy
+            companion (Pet).
           </p>
           <div>
             <FaChevronCircleRight className="icon-5" />
@@ -47,9 +58,9 @@ const Home = () => {
           </div>
           <h4>Pet Grooming</h4>
           <p>
-            Welcome to Pets First web page. We are committed
-            to enhance your health by ensuring that you are always safe and have
-            a happy companion (Pet).
+            Welcome to Pets First web page. We are committed to enhance your
+            health by ensuring that you are always safe and have a happy
+            companion (Pet).
           </p>
           <div>
             <FaChevronCircleRight className="icon-6" />
