926008a904a2ef8c74e6f70e3361c324be4e98b0
leroy-netizen <leroythomas051@gmail.com>
2022-09-06 08:34:29 +0300

    implement delete request | set cors on server

--- client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
+++ client/says-my-vet/src/pages/RegisterDoc/RegDoc.page.js
@@ -1,18 +1,42 @@
-import React, {useState} from 'react'
-import "./RegDoc.styles.css"
+import React, { useState } from 'react'
+
 
 const RegDoc = () => {
   // state for controlled input fields
   const [name, setName] = useState('')
-  const [licenceNo, setLicenceNo] = useState('')
   const [location, setLocation] = useState('Nairobi')
+  const [license_number, setLicenceNumber] = useState(11111111)
   const [specialization, setSpecialization] = useState('Dentistry')
-  const [phone, setPhone] = useState('')
-  const [picLink, setPicLink] = useState('')
-  const [experience, setExperience]= useState('')
+  const [phone_number, setPhone_number] = useState(17955457183)
+  const [picture_link, setpicture_link] = useState('fygggfff')
+  const [years_of_experience, setYears_of_experience] = useState(2)
+  const [toasty, setToasty] = useState(null)
+  
+
+  // const history = useNavigate()
+// handle form submission
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const doctor = {name, license_number, location, specialization, phone_number, picture_link, years_of_experience}
+    console.log(doctor)
+    // notify()
+    // make post request
+    fetch('http://localhost:9292/doctors', {
+      method: 'POST',
+      headers: { "Content-Type": 'application/json' },
+      body: JSON.stringify(doctor)
+  
+    }).then(() => {
+      console.log("New doctor added successfuly");
+    })
+    // history.push('/')
+  }
+
   return (
-<div className='registerDoctors'>
-  <form className="myForm" > 
+    
+    <div className='registerDoctors'>
+  <form className="myForm" onSubmit={handleSubmit} > 
     <h2>Register with Us</h2>
     <div className="register_doctors">
       <div className='register_doctorsA'>
@@ -20,7 +44,7 @@ const RegDoc = () => {
           <input type="text" className="field" placeholder="name" value={name} onChange={(e)=> setName(e.target.value) } />
         </label>
         <label>License_number:
-              <input type="text" className="field" placeholder="license_number" value={licenceNo} onChange={ (e)=> setLicenceNo(e.target.value)} />
+              <input type="text" className="field" placeholder="license_number" value={license_number} onChange={ (e)=> setLicenceNumber(e.target.value)} />
         </label>
       </div>
     <div className='register_doctorsA'>
@@ -44,15 +68,15 @@ const RegDoc = () => {
     </div>
     <div className='register_doctorsA'>
     <label>Phone Number:
-              <input type="text" value={phone} className="field" placeholder="phone_number" onChange={(e) => setPhone(e.target.value)} />
+              <input type="text" value={phone_number} className="field" placeholder="phone_number" onChange={(e) => setPhone_number(e.target.value)} />
             </label>
     <label>Picture Link:
-              <input type="text" value={picLink} className="field" placeholder="link to face-image" onChange={(e) => setPicLink(e.target.value)} />
+              <input type="text" value={picture_link} className="field" placeholder="link to face-image" onChange={(e) => setpicture_link(e.target.value)} />
     </label>
     </div>
     <div className='register_doctorsA'>
-    <label>Years of Experience
-    <select value={experience} onChange={(e)=> {setExperience(e.target.value)}}>
+    <label>Years of years_of_experience
+    <select value={years_of_experience} onChange={(e)=> {setYears_of_experience(e.target.value)}}>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -64,18 +88,20 @@ const RegDoc = () => {
     </div>
     <div className='submitBtn'>
     <button type="submit" className="registerbtn"> Submit</button>    
-    </div>                  
+          </div>   
+      
       </div>
       </form> 
-      <p>{name}</p>
-      <p>{licenceNo}</p>
+      {/* <p>{name}</p>
+      <p>{licence_number}</p>
       <p>{picLink}</p>
-      <p>{phone}</p>
+      <p>{phone_number}</p>
       <p>{specialization}</p>
       <p>{location}</p>
-      <p>{experience}</p>
+      <p>{years_of_experience}</p> */}
+    
       
-</div>
+    </div>
   )
 }
 
--- client/says-my-vet/src/pages/ViewDoc/DoctorDetails/DoctorDetails.js
+++ client/says-my-vet/src/pages/ViewDoc/DoctorDetails/DoctorDetails.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import useFetch from '../../../custom-hooks/useFetch'
+// const history = useHistory()
 
 const DoctorDetails = () => {
-    const {id} = useParams()
+
+  const { id } = useParams()
+  const {data: doctor, error, loading } = useFetch('http://localhost:9292/doctors/' + id)
+
+  const handleDelete = () => {
+    fetch('http://localhost:9292/doctors/' + doctor.id, {
+      // mode: 'no-cors',
+      method: 'DELETE'
+    }).then(()=>{
+      console.log(id)
+    })
+  }
   return (
-      <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          DoctorDetails {id} </div>
+    <div>
+      {
+        doctor && (
+      <div className="container">
+       <img src={doctor.picture_link} alt={doctor.name} />
+        <div className="img">{doctor.name}
+        </div>
+        </div>
+        )
+      }
+      <button onClick={handleDelete}>delete</button>
+    </div>
   )
 }
 
